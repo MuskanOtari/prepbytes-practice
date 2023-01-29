9011ec875d0b43abeabb6e45d52b704745ec6617
muskan <muskanotari1999@gmail.com>
2023-01-29 22:38:29 +0530

    Added Javascript Assignment-1 to github

---
+++ JAVASCRIPT/LiveClasses/index1.js
@@ -0,0 +1,26 @@
+//variable declaration
+var a = 20;
+console.log(a);
+console.log(a);
+console.log(a);
+//const declaration
+const myName = 10;
+console.log(myName);
+
+
+//declaration => It means creating of a container and giving name to it
+var b;
+//initialization => It means giving first value to conainer
+b = 10;
+//assigning => Assignment means to assign a value again to container we can assign values several times to container
+b = 100;
+
+//undefined  vs  notdefined
+
+//undefined => declared but not assigned or initialized 
+//when we tries to print the value before it's defining it gives us undefined
+
+console.log(c); //try to run this o/p will be== undefined
+
+//notdefined =>  not declared the variable
+//
